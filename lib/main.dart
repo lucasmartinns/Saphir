03580d0b82bd5557cada273d0d366ad15adfadd3
lucasmartinns <lucasalvesmartinns@gmail.com>
2024-09-12 17:19:21 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(0, 0, 0, 0)),
       useMaterial3: true),
-      home: Login(),
+      home: const Login(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -7,67 +7,121 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(0, 30, 30, 30),
-      // appBar: AppBar(title: const Text("Saphir"),
-      // backgroundColor: Colors.blueGrey,),
+      backgroundColor:
+          const Color.fromARGB(255, 30, 30, 30), // Fundo preto opaco
       body: Center(
-          child: Form(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Center(
-                    child: Text("Saphir",
-                    style: TextStyle(color: Color.fromARGB(255, 23, 44, 228),
-                    fontSize: 50,
-                    fontFamily: 'DaysOne'),),
+        child: Form(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Center(
+                  child: Text(
+                    "saphir",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 23, 44, 228),
+                        fontSize: 50,
+                        fontFamily: 'DaysOne'),
                   ),
-                  const SizedBox(height: 60),
-                  
-                   TextFormField(
-                    autofocus: true,
+                ),
+                const SizedBox(height: 60),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: TextFormField(
+                    autofocus: true, // Apenas um campo com autofocus
                     decoration: const InputDecoration(
-                      filled: true, //preenche o fundo
-                      fillColor: Colors.white,
-                      iconColor: Color.fromARGB(255, 23, 44, 228),
-                      hintText: "E-mail",
-                      border: OutlineInputBorder(
-                        borderRadius:  BorderRadius.all(Radius.circular(30)),
-                        borderSide: BorderSide.none
-                      )
-                    ),
-                   ),
-                   const  SizedBox(height: 40),
-
-                    TextFormField(
-                    autofocus: true,
+                        filled: true, // Preenche o fundo
+                        fillColor: Colors.white,
+                        iconColor: Color.fromARGB(255, 23, 44, 228),
+                        hintText: "E-mail",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            borderSide: BorderSide.none)),
+                  ),
+                ),
+                const SizedBox(height: 40),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: TextFormField(
+                    // autofocus removido deste campo
                     decoration: const InputDecoration(
-                      filled: true, //preenche o fundo
-                      fillColor: Colors.white,
-                      iconColor: Color.fromARGB(255, 23, 44, 228),
-                      hintText: "Senha",
-                       border: OutlineInputBorder(
-                        borderRadius:  BorderRadius.all(Radius.circular(30)),
-                        borderSide: BorderSide.none
-                      )
+                        filled: true, // Preenche o fundo
+                        fillColor: Colors.white,
+                        iconColor: Color.fromARGB(255, 23, 44, 228),
+                        hintText: "Senha",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            borderSide: BorderSide.none)),
+                    obscureText: true, // Para ocultar a senha
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(
+                      right: 40,
                     ),
-                   ),
-                  const SizedBox(height: 10),
-                  Center(
-                    child: ElevatedButton(onPressed: (){}, child:Text("Entrar"),
-                    style: ElevatedButton.styleFrom(
-                      fixedSize: Size(100, 50),
-                      backgroundColor: Color.fromARGB(255, 23, 44, 228),
-                      foregroundColor: Colors.white,
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                          onPressed: () {
+                            // Função de esquecimento de senha
+                          },
+                          child: const Text(
+                            "Esqueceu a senha?",
+                            style: TextStyle(color: Colors.blue),
+                          )),
+                    )),
+                const SizedBox(height: 40),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Função de login
+                    },
+                    child: const Text("Entrar",
+                        style: TextStyle(color: Colors.white)),
+                    style: OutlinedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 170, vertical: 15),
+                        side: const BorderSide(color: Colors.white, width: 2),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        backgroundColor: Colors.black),
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child:Container(
+                        width: 60,
+                        height: 60,
+                        child: Image.asset('assets/image/google.png')),
+                    ),
                     
-                    ),),
-                  ),
-                ],
-              ),
+                    SizedBox(width: 30),
+                    Container(
+                        width: 60,
+                        height: 60,
+                        child: Image.asset('assets/image/apple.png')),
+                    const SizedBox(width: 30),
+                    Container(
+                        width: 60,
+                        height: 60,
+                        child: Image.asset('assets/image/facebook.png')),
+                  ],
+                ),
+                const SizedBox(height: 30,),
+
+               const  Divider()
+              ],
             ),
           ),
         ),
-      );
-}
+      ),
+    );
+  }
 }
